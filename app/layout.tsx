8b7967147281c3a5e864bac9aa56955370e4aeda
Dipanshu Goyal <dipanshugoyal975@gmail.com>
2023-06-27 02:01:39 +0530

    Fetching listings from database from server component and displaying on client side

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,9 @@ export default async function RootLayout({
           <RegisterModel /> {/*isOpen - same as isOpen = true */} 
           <Navbar currentUser={currentUser}/>
         </ClientOnly>
-        {children}
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
       </body>
     </html>
   )
